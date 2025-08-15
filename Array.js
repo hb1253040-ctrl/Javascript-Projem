89d2655250f0e400a2c44c925590d617fb74b126
hb1253040-ctrl <hb1253040@gmail.com>
2025-08-15 19:53:37 +0300

    Diziler

---
+++ Array.js
@@ -0,0 +1,22 @@
+let urun1 = "İphone 12";
+let urun2 = "İphone 13";
+let urun3 = "İphone 14";
+
+let urunler =  ["iphone 12", "iphone 13", "iphone 14"];
+let fiyatlar = [9000,10000,20000];
+let renkler =  ["siyah", "beyaz", "kırmızı"];
+
+/*
+console.log(urunler[0]);
+console.log(urunler[1]);
+console.log(urunler[2]);
+*/
+
+console.log(`${urunler[0]}-${fiyatlar[0]}-${renkler[0]}`);
+console.log(`${urunler[1]}-${fiyatlar[1]}-${renkler[1]}`);
+console.log(`${urunler[2]}-${fiyatlar[2]}-${renkler[2]}`);
+
+let kursAdi = "Komple Web Geliştirme Eğitimi";
+console.log(kursAdi[5]);
+console.log(kursAdi.split(" "));
+
